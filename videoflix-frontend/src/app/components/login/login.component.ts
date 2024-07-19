39f30906cde02061232755ed41eb1c3ef10d0ce1
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-07-19 10:30:48 +0200

    Login mask and safe Token

--- videoflix-frontend/src/app/components/login/login.component.ts
+++ videoflix-frontend/src/app/components/login/login.component.ts
@@ -6,13 +6,35 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = ';'
+  username: string = '';
+  password: string = '';
 
-  login(){
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
 
+    const raw = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+    try{
+    let resp = await fetch('http://127.0.0.1:8000/login/', requestOptions);
+    let json = await resp.json();
+    localStorage.setItem('token', json.token);
+    //redirect
+    } catch(e){
+      //show Error message
+      console.error(e);
+    }
   }
 }
