4cc7e4fbe7cbddf032d46f7f8cb669b9bf5cbce1
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-07-21 09:43:32 +0200

    set landing page and links to login & registration

--- videoflix-frontend/src/app/app.routes.ts
+++ videoflix-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
-  {path: '', component: LoginComponent },
+  {path: '', component: LandingPageComponent },
+  {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent},
 ];
---
+++ videoflix-frontend/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+  emailForRegistration: string = ''
+
+  constructor(private router: Router) {
+
+   }
+
+  navigateToRegistration(){
+    this.router.navigate(['/register'])
+  }
+
+  navigateToLogin(){
+    this.router.navigate(['/login'])
+  }
+}
