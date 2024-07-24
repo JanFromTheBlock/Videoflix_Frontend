51104c6ba7f86e6e97de0c4590b00b33786b4b5a
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-07-24 16:51:43 +0200

    error handling part1

--- videoflix-frontend/src/app/components/register/register.component.ts
+++ videoflix-frontend/src/app/components/register/register.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
   username: string = '';
@@ -17,11 +18,14 @@ export class RegisterComponent {
   password2: string = '';
   show1: boolean = false;
   show2: boolean = false;
+  usernameError: string = '';
+  emailError: string = '';
+  serverError: string = '';
 
   constructor(private as: AuthService, private router: Router) {}
 
-  navigateToLogin(){
-    this.router.navigate(['/login'])
+  navigateToLogin() {
+    this.router.navigate(['/login']);
   }
 
   showPassword(passwordNumber: any) {
@@ -31,29 +35,49 @@ export class RegisterComponent {
     if (passwordNumber == 2) {
       this.show2 = !this.show2;
     }
+  }
 
-}
-
-  async registerUser(){
-    if(this.password1 === this.password2){
+  async registerUser() {
+    if (this.password1 === this.password2) {
       await this.submitRegistration();
     }
   }
 
-  async submitRegistration(){
-    try{
-      let response = await this.as.ResponseRegister(this.username, this.email, this.password1);
+  async submitRegistration() {
+    try {
+      let response = await this.as.ResponseRegister(
+        this.username,
+        this.email,
+        this.password1
+      );
       console.log(response);
-        //REDIRECT
-      }catch (e){
-      console.error('an error occured', e);
+      //REDIRECT
+    } catch (e: any) {
+      if (e && e.error && e.error.errors) {
+        if (e.error.errors.username) {
+          this.usernameError = e.error.errors.username;
+        }
+        if (e.error.errors.email) {
+          this.emailError = e.error.errors.email;
+        } else if (
+          e.status === 0 ||
+          e.status === 400 ||
+          (e.message && e.message.includes('ERR_CONNECTION_REFUSED'))
+        ) {
+          this.serverError = 'Server not reachable. Please try again later.';
+        }
+      }else{
+        this.serverError = 'An unexpected error occurred. Please try again later.';
+      }
     }
   }
 
-  validateRegisterForm(){
+  validateRegisterForm() {
     const form: any = document.getElementById('register-form');
     const submitBtn: any = document.getElementById('button-register');
-    const inputs = form.querySelectorAll('input[type="text"], input[type="email"], input[type="password"]');
+    const inputs = form.querySelectorAll(
+      'input[type="text"], input[type="email"], input[type="password"]'
+    );
     const emailInput = document.getElementById('new-email') as HTMLInputElement;
     let email = emailInput.value;
     const pw1Input = document.getElementById('password1') as HTMLInputElement;
@@ -62,17 +86,16 @@ export class RegisterComponent {
     let pw2 = pw2Input.value;
     let allFieldsFilled = true;
 
-    inputs.forEach((input: { value: string; }) => {
-        if (!input.value.trim() || !this.isValidEmail(email) || pw1 != pw2) {
-            allFieldsFilled = false;
-        }
+    inputs.forEach((input: { value: string }) => {
+      if (!input.value.trim() || !this.isValidEmail(email) || pw1 != pw2) {
+        allFieldsFilled = false;
+      }
     });
     submitBtn.disabled = !allFieldsFilled;
   }
 
   isValidEmail(email: any) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailPattern.test(email)
-}
-
+    return emailPattern.test(email);
+  }
 }
